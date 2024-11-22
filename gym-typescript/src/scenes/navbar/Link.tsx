e1b6487828646a548b830ea4ee4e3d964e62e68b
FeCharaf <felipecharaf@gmail.com>
2024-11-21 23:12:53 -0300

    Navbar complete

--- gym-typescript/src/scenes/navbar/Link.tsx
+++ gym-typescript/src/scenes/navbar/Link.tsx
@@ -1,19 +1,24 @@
-import AnchorLink from "react-anchor-link-smooth-scroll"
+import AnchorLink from "react-anchor-link-smooth-scroll";
 
 type Props = {
-    page: string;
-}
+  page: string;
+  selectedPage: string;
+  setSelectedPage: (value: string) => void;
+};
 
-function Link({page}: Props) {
-    const lowerCasePage
+const Link = ({ page, selectedPage, setSelectedPage }: Props) => {
+  // Remove spaces and Lower Case
+  const lowerCasePage = page.toLowerCase().replace(/ /g, "");
+
   return (
-    <AnchorLink className={}
-    href={}
-    onClick={}>
-        {page}
+    <AnchorLink
+      className={`${selectedPage === lowerCasePage && "text-primary-500"} transition duration-500 hover:text-primary-300`}
+      href={`#${lowerCasePage}`}
+      onClick={() => setSelectedPage(lowerCasePage)}
+    >
+      {page}
     </AnchorLink>
-  )
-}
+  );
+};
 
-export default Link
-
+export default Link;

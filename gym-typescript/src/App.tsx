ad4cd1170b05dfc7479d34e61402ac7908a5578d
FeCharaf <felipecharaf@gmail.com>
2024-11-27 15:28:45 -0300

    Images Carrossel

--- gym-typescript/src/App.tsx
+++ gym-typescript/src/App.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { SelectedPage } from "@/shared/types";
 import Home from "@/scenes/home";
 import Benefits from "@/scenes/benefits";
+import OurClasses from "./scenes/ourClasses";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(
@@ -14,6 +15,7 @@ function App() {
       <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
       <Home />
       <Benefits />
+      <OurClasses />
     </div>
   );
 }
---
+++ gym-typescript/src/scenes/ourClasses/index.tsx
@@ -0,0 +1,60 @@
+import Class from "./Class";
+import image1 from "@/assets/image1.png";
+import image2 from "@/assets/image2.png";
+import image3 from "@/assets/image3.png";
+import image4 from "@/assets/image4.png";
+import image5 from "@/assets/image5.png";
+import image6 from "@/assets/image6.png";
+
+const OurClasses = () => {
+  return (
+    <section>
+      <div className="mx-auto w-5/6">
+        <h1 className="py-8 text-3xl font-bold">OUR CLASSES</h1>
+        <p className="w-3/5">
+          Fringilla a sed at suspendisse ut enim volutpat. Rhoncus vel est
+          tellus quam porttitor. Mauris velit euismod elementum arcu neque
+          facilisi. Amet semper tortor facilisis metus nibh. Rhoncus sit enim
+          mattis odio in risus nunc.
+        </p>
+      </div>
+
+      <div className="mt-10 h-[353px] w-full overflow-x-auto overflow-y-hidden">
+        <ul className="w-[3000px] ">
+          <Class
+            name="Weight Training Classes"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+            image={image1}
+          />
+          <Class
+            name="Weight Training Classes"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+            image={image2}
+          />
+          <Class
+            name="Weight Training Classes"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+            image={image3}
+          />
+          <Class
+            name="Weight Training Classes"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+            image={image4}
+          />
+          <Class
+            name="Weight Training Classes"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+            image={image5}
+          />
+          <Class
+            name="Weight Training Classes"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+            image={image6}
+          />
+        </ul>
+      </div>
+    </section>
+  );
+};
+
+export default OurClasses;

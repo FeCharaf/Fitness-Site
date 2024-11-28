3a303fc708f8b9e7c67725d534398a3b307b6ec6
FeCharaf <felipecharaf@gmail.com>
2024-11-28 17:11:52 -0300

    contactUs complete

--- gym-typescript/src/scenes/contactUs/index.tsx
+++ gym-typescript/src/scenes/contactUs/index.tsx
@@ -1,11 +1,11 @@
 import image from "@/assets/ContactUsPageGraphic.png";
 
 const ContactUs = () => {
-    const inputStyles = `mb-5 rounded-lg bg-primary-300
-  px-5 py-3 placeholder-white`
+  const inputStyles = `mb-5 rounded-lg bg-primary-300
+  px-5 py-3 placeholder-white`;
   return (
     <section className="py-24">
-      <div className="flex mx-auto w-5/6 h-[800px] border-2">
+      <div className="mx-auto flex h-[800px] w-5/6">
         <div>
           <h1 className="font-montserrat text-3xl font-bold">
             <strong className="text-primary-500">JOIN NOW</strong> TO GET IN
@@ -17,15 +17,25 @@ const ContactUs = () => {
             adipiscing leo egestas nisi elit risus sit. Nunc cursus sagittis.
           </p>
 
-          <form className="flex-col flex gap-5">
+          <form className="flex flex-col gap-5">
             <input className={inputStyles} type="text" placeholder="NAME" />
             <input className={inputStyles} type="text" placeholder="EMAIL" />
             <input className={inputStyles} type="text" placeholder="MESSAGE" />
+            <button
+              type="submit"
+              className="w-1/4 rounded-lg bg-secondary-500 px-20 py-3 transition duration-500 hover:text-white"
+            >
+              SUBMIT
+            </button>
           </form>
         </div>
 
-        <div className="w-1/2 flex items-center justify-center">
-          <img src={image} className="w-full max-w-[460px] rounded-lg" alt="Contact Us Illustration"></img>
+        <div className="flex w-1/2 items-center justify-center">
+          <img
+            src={image}
+            className="mr-10 w-full max-w-[460px] rounded-lg"
+            alt="Contact Us Illustration"
+          ></img>
         </div>
       </div>
     </section>

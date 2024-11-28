ca84a0619cd626ef626d79ab4f9ebaa1865d16d3
FeCharaf <felipecharaf@gmail.com>
2024-11-28 17:16:12 -0300

    Footer import

---
+++ gym-typescript/src/scenes/footer/index.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer className="bg-primary-100 py-16">
+            <div className="w-5/6 mx-auto border-2">FOoter</div>
+        </footer>
+    )
+}
+
+export default Footer

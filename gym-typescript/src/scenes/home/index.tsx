1b01267676c59d1d50243b0080c4f2921ae743b6
FeCharaf <felipecharaf@gmail.com>
2024-12-10 20:59:18 -0300

    Link bug fix

--- gym-typescript/src/scenes/home/index.tsx
+++ gym-typescript/src/scenes/home/index.tsx
@@ -36,6 +36,7 @@ const Home = ({ setSelectedPage }: Props) => {
               <AnchorLink
                 className="text-sm font-bold text-primary-500 underline hover:text-secondary-500"
                 onClick={() => setSelectedPage(SelectedPage.ContactUs)}
+                href={`#${SelectedPage.ContactUs}`}
               >
                 <p>Learn More</p>
               </AnchorLink>

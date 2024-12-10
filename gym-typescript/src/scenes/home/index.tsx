07e595d8d71b8122d344badc596ca300bc819ee6
FeCharaf <felipecharaf@gmail.com>
2024-12-10 20:53:34 -0300

    Responsive Design for HomePage

--- gym-typescript/src/scenes/home/index.tsx
+++ gym-typescript/src/scenes/home/index.tsx
@@ -1,48 +1,64 @@
+import useMediaQuery from "@/hooks/useMediaQuery";
 import { SelectedPage } from "@/shared/types";
+import ActionButton from "@/shared/ActionButton";
+
 import HomeText from "@/assets/HomePageText.png";
 import HomeGraph from "@/assets/HomePageGraphic.png";
 import EvolveText from "@/assets/EvolveText.png";
 import SponsorRedBull from "@/assets/SponsorRedBull.png";
 import SponsorForbes from "@/assets/SponsorForbes.png";
 import SponsorFortune from "@/assets/SponsorFortune.png";
-import ActionButton from "@/shared/ActionButton";
+import AnchorLink from "react-anchor-link-smooth-scroll";
 
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
 };
 
 const Home = ({ setSelectedPage }: Props) => {
+  const isAboveMediumScreens = useMediaQuery("(min-width:1060px)");
+
   return (
-    <section id="home" className="justify-center bg-gray-20 pb-10">
-      <div className="flex h-3/4 w-full items-center justify-center py-10">
+    <section id="home" className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0">
+      <div className="flex items-center justify-center">
         <div>
-          <img src={EvolveText} className="w-1/3"></img>
-          <img src={HomeText}></img>
+          <img src={EvolveText} alt="evolve text" className="w-1/3"></img>
+          <img src={HomeText} alt="homepage text"></img>
           <div className="w-4/5">
-            <p className="mb-8 mt-8 text-sm">
+            <p className="mt-8 text-sm">
               Unrivaled Gym. Unparalleled Training Fitness Classes. World Class
               Studios to get the Body Shapes That you Dream of.. Get Your Dream
               Body Now.
             </p>
-            <ActionButton setSelectedPage={setSelectedPage}>
-              Join Now
-            </ActionButton>
-            <a href="#">Learn More</a>
+            <div className="mt-8 flex items-center gap-8">
+              <ActionButton setSelectedPage={setSelectedPage}>
+                Join Now
+              </ActionButton>
+              <AnchorLink
+                className="text-sm font-bold text-primary-500 underline hover:text-secondary-500"
+                onClick={() => setSelectedPage(SelectedPage.ContactUs)}
+              >
+                <p>Learn More</p>
+              </AnchorLink>
+            </div>
           </div>
         </div>
         <div>
           <img src={HomeGraph}></img>
         </div>
       </div>
-      <div className="h-[150px] w-full bg-primary-100 py-10">
-        <div className="mx-auto w-5/6">
-          <div className="flex w-3/5 items-center justify-between gap-8">
-            <img alt="redbull-sponsor" src={SponsorRedBull} />
-            <img alt="forbes-sponsor" src={SponsorForbes} />
-            <img alt="fortune-sponsor" src={SponsorFortune} />
+
+      {/* SPONSORS */}
+      {isAboveMediumScreens && (
+        <div className="h-[150px] w-full bg-primary-100 py-10">
+          <div className="mx-auto w-5/6">
+            <div className="flex w-3/5 items-center justify-between gap-8">
+              <img alt="redbull-sponsor" src={SponsorRedBull} />
+              <img alt="forbes-sponsor" src={SponsorForbes} />
+              <img alt="fortune-sponsor" src={SponsorFortune} />
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </section>
   );
 };

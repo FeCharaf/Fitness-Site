1f64dee6dfe56ce475bdc6451a898da662c59223
FeCharaf <felipecharaf@gmail.com>
2024-12-19 10:47:25 -0300

    Benefits responsive layout

--- gym-typescript/src/scenes/benefits/index.tsx
+++ gym-typescript/src/scenes/benefits/index.tsx
@@ -1,5 +1,8 @@
 import Benefit from "./Benefit";
+import HText from "@/shared/HText";
 import BenefitsGraphic from "@/assets/BenefitsPageGraphic.png";
+import { motion } from "framer-motion";
+import { BenefitType, SelectedPage } from "@/shared/types";
 
 import {
   HomeModernIcon,
@@ -7,42 +10,57 @@ import {
   AcademicCapIcon,
 } from "@heroicons/react/24/solid";
 
-const Benefits = () => {
+const benefits: Array<BenefitType> = [
+  {
+    icon: <HomeModernIcon className="h-6 w-6" />,
+    title: "State of the Art Facilities",
+    description: "Neque adipiscing amet amet enim. Feugiat dolor enim fermentum in a in lectus pellentesque. Ullamcorper et."
+  },
+  {
+    icon: <UserGroupIcon className="h-6 w-6" />,
+    title: "100's of Diverse Classes",
+    description: "Eu ipsum id egestas risus tempus enim semper felis quis. Nec consectetur ac venenatis facilisi est. Eget ac turpis id."
+  },
+  {
+    icon: <AcademicCapIcon className="h-6 w-6" />,
+    title: "Expert and Pro Trainers",
+    description: "Fusce vestibulum aliquam ut cras. Nisl lectus egestas sapien nisl. Lacus at mi sit pellentesque. Congue parturient."
+  }
+]
+
+type Props = {
+  setSelectedPage: (value: SelectedPage) => void;
+};
+
+const Benefits = ({ setSelectedPage }: Props) => {
   return (
     <section id="benefits" className="w-full pb-20">
-      <div className="mx-auto w-5/6 items-center justify-center py-20">
-        <h1 className="font-montserrat text-3xl font-bold mb-6">
-          MORE THAN JUST GYM
-        </h1>
-        <p>
-          We provide world class fitness equipment, trainers and classes to get
-          you to your ultimate fitness goals with ease. We provide true care
-          into each and every member.
-        </p>
-        <div className="flex gap-8">
-          <Benefit
-            icon="ICO"
-            title="State of the Art Facilities"
-            text="Neque adipiscing amet amet enim. Feugiat dolor enim fermentum in a in lectus pellentesque. Ullamcorper et."
-            link="Learn More"
-          />
-          <Benefit
-            icon="ICO"
-            title="100's of Diverse Classes"
-            text="Eu ipsum id egestas risus tempus enim semper felis quis. Nec consectetur ac venenatis facilisi est. Eget ac turpis id."
-            link="Learn More"
-          />
-          <Benefit
-            icon="ICO"
-            title="Expert and Pro Trainers"
-            text="Fusce vestibulum aliquam ut cras. Nisl lectus egestas sapien nisl. Lacus at mi sit pellentesque. Congue parturient."
-            link="Learn More"
-          />
+      <motion.div
+        className="mx-auto w-5/6 items-center justify-center py-20"
+        onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)}
+      >
+        {/* HEADER SECTION */}
+        <div className="md:my-5 md:w-3/5">
+          <HText>MORE THAN JUST GYM.</HText>
+          <p>
+            We provide world class fitness equipment, trainers and classes to
+            get you to your ultimate fitness goals with ease. We provide true
+            care into each and every member.
+          </p>
         </div>
-      </div>
+
+        {/* BENEFITS */}
+        <div className="mt-5 items-center justify-between gap-8 md:flex">
+            {benefits.map()})}
+        </div>
+      </motion.div>
 
       <div className="mx-auto flex w-5/6 gap-20">
-        <img src={BenefitsGraphic} alt="benefits-page-graphic" className="max-w-[500px] max-h-[500px]" />
+        <img
+          src={BenefitsGraphic}
+          alt="benefits-page-graphic"
+          className="max-h-[500px] max-w-[500px]"
+        />
 
         <div className="my-auto w-3/4">
           <h1 className="text-3xl font-bold">

461cf566d3ea033c5daaeaa206ffe60df5a21d43
FeCharaf <felipecharaf@gmail.com>
2024-12-19 21:51:32 -0300

    Benefit type anchor

--- gym-typescript/src/scenes/benefits/Benefit.tsx
+++ gym-typescript/src/scenes/benefits/Benefit.tsx
@@ -1,17 +1,28 @@
+import { SelectedPage } from "@/shared/types";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+
 type Props = {
-  icon: any;
+  icon: JSX.Element;
   title: string;
-  text: string;
-  link: string;
+  description: string;
+  setSelectedPage: (value: SelectedPage) => void;
 };
 
-const Benefit = ({ icon, title, text, link }: Props) => {
+const Benefit = ({ icon, title, description, setSelectedPage }: Props) => {
   return (
-    <div className="border-2 border-gray-100 items-center p-16 m-10 justify-center mx-auto flex flex-col gap-4">
-      <div className="border-2 rounded-full border-gray-100 bg-primary-100 p-4 ">{icon}</div>
-      <h4 className="font-bold text-xl">{title}</h4>
-      <p>{text}</p>
-      <a href={link}>Learn More</a>
+    <div className="m-10 mx-auto flex flex-col items-center justify-center gap-4 border-2 border-gray-100 p-16">
+      <div className="rounded-full border-2 border-gray-100 bg-primary-100 p-4 ">
+        {icon}
+      </div>
+      <h4 className="text-xl font-bold">{title}</h4>
+      <p>{description}</p>
+      <AnchorLink
+        className="text-sm font-bold text-primary-500 underline hover:text-secondary-500"
+        onClick={() => setSelectedPage(SelectedPage.ContactUs)}
+        href={`#${SelectedPage.ContactUs}`}
+      >
+        <p>Learn More</p>
+      </AnchorLink>
     </div>
   );
 };
--- gym-typescript/src/scenes/benefits/index.tsx
+++ gym-typescript/src/scenes/benefits/index.tsx
@@ -9,24 +9,28 @@ import {
   UserGroupIcon,
   AcademicCapIcon,
 } from "@heroicons/react/24/solid";
+import { text } from "stream/consumers";
 
 const benefits: Array<BenefitType> = [
   {
     icon: <HomeModernIcon className="h-6 w-6" />,
     title: "State of the Art Facilities",
-    description: "Neque adipiscing amet amet enim. Feugiat dolor enim fermentum in a in lectus pellentesque. Ullamcorper et."
+    description:
+      "Neque adipiscing amet amet enim. Feugiat dolor enim fermentum in a in lectus pellentesque. Ullamcorper et.",
   },
   {
     icon: <UserGroupIcon className="h-6 w-6" />,
     title: "100's of Diverse Classes",
-    description: "Eu ipsum id egestas risus tempus enim semper felis quis. Nec consectetur ac venenatis facilisi est. Eget ac turpis id."
+    description:
+      "Eu ipsum id egestas risus tempus enim semper felis quis. Nec consectetur ac venenatis facilisi est. Eget ac turpis id.",
   },
   {
     icon: <AcademicCapIcon className="h-6 w-6" />,
     title: "Expert and Pro Trainers",
-    description: "Fusce vestibulum aliquam ut cras. Nisl lectus egestas sapien nisl. Lacus at mi sit pellentesque. Congue parturient."
-  }
-]
+    description:
+      "Fusce vestibulum aliquam ut cras. Nisl lectus egestas sapien nisl. Lacus at mi sit pellentesque. Congue parturient.",
+  },
+];
 
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
@@ -51,7 +55,15 @@ const Benefits = ({ setSelectedPage }: Props) => {
 
         {/* BENEFITS */}
         <div className="mt-5 items-center justify-between gap-8 md:flex">
-            {benefits.map()})}
+          {benefits.map((benefit: BenefitType) => (
+            <Benefit
+              key={benefit.title}
+              icon={benefit.icon}
+              title={benefit.title}
+              description={benefit.description}
+              setSelectedPage={setSelectedPage}
+            />
+          ))}
         </div>
       </motion.div>
 

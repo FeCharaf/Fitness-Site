342661f3bee896a7ab8ad9e21876d5dadd34fcb4
FeCharaf <felipecharaf@gmail.com>
2024-12-23 18:51:49 -0300

    little animations for our classes

--- gym-typescript/src/scenes/ourClasses/index.tsx
+++ gym-typescript/src/scenes/ourClasses/index.tsx
@@ -5,6 +5,7 @@ import image3 from "@/assets/image3.png";
 import image4 from "@/assets/image4.png";
 import image5 from "@/assets/image5.png";
 import image6 from "@/assets/image6.png";
+import HText from "@/shared/HText";
 import { SelectedPage } from "@/shared/types";
 import { motion } from "framer-motion";
 
@@ -15,19 +16,34 @@ type Props = {
 const OurClasses = ({ setSelectedPage }: Props) => {
   return (
     <section id="ourclasses" className="bg-primary-100 py-20">
-      <motion.div onViewportEnter={() => setSelectedPage(SelectedPage.OurClasses)}>
-        <div className="mx-auto w-5/6">
-          <h1 className="py-8 text-3xl font-bold">OUR CLASSES</h1>
-          <p className="w-3/5">
-            Fringilla a sed at suspendisse ut enim volutpat. Rhoncus vel est
-            tellus quam porttitor. Mauris velit euismod elementum arcu neque
-            facilisi. Amet semper tortor facilisis metus nibh. Rhoncus sit enim
-            mattis odio in risus nunc.
-          </p>
-        </div>
+      <motion.div
+        onViewportEnter={() => setSelectedPage(SelectedPage.OurClasses)}
+      >
+        <motion.div
+          className="mx-auto w-5/6"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }} //amount is the percentage of the div that you can see to proc the animation
+          transition={{ duration: 0.5 }} // duration of the animation
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <div className="md:w-3/5">
+            <HText>OUR CLASSES</HText>
+            <p className="w-3/5">
+              Fringilla a sed at suspendisse ut enim volutpat. Rhoncus vel est
+              tellus quam porttitor. Mauris velit euismod elementum arcu neque
+              facilisi. Amet semper tortor facilisis metus nibh. Rhoncus sit enim
+              mattis odio in risus nunc.
+            </p>
+          </div>
+          
+        </motion.div>
 
         <div className="mt-10 h-[353px] w-full overflow-x-auto overflow-y-hidden">
-          <ul className="w-[3000px] ">
+          <ul className="w-[2200px] whitespace-nowrap">
             <Class
               name="Weight Training Classes"
               description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."

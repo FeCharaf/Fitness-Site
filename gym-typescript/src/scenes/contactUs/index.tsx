de561a0fc2b796d05d57e7841e9bc5f041062064
FeCharaf <felipecharaf@gmail.com>
2025-01-07 18:50:51 -0300

    Our Classes forms

--- gym-typescript/src/scenes/contactUs/index.tsx
+++ gym-typescript/src/scenes/contactUs/index.tsx
@@ -21,7 +21,7 @@ const ContactUs = ({ setSelectedPage }: Props) => {
       e.preventDefault();
     }
   };
-  const inputStyles = `mb-5 rounded-lg bg-primary-300
+  const inputStyles = `mb-5 w-full rounded-lg bg-primary-300
   px-5 py-3 placeholder-white`;
   return (
     <section id="contactus" className="mx-auto w-5/6 pb-32 pt-24">
@@ -65,30 +65,96 @@ const ContactUs = ({ setSelectedPage }: Props) => {
               visible: { opacity: 1, y: 0 },
             }}
           >
-            <form target="_blank" onSubmit={onSubmit} method="POST" action="https://formsubmit.co/felipecharaf@gmail.com">
-              <input className={inputStyles} type="text" placeholder="NAME" />
-              <input className={inputStyles} type="text" placeholder="EMAIL" />
+            <form
+              target="_blank"
+              onSubmit={onSubmit}
+              method="POST"
+              action="https://formsubmit.co/92bde390317b198d3719fbac1edb21a5"
+            >
               <input
                 className={inputStyles}
                 type="text"
-                placeholder="MESSAGE"
+                placeholder="NAME"
+                {...register("name", {
+                  required: true,
+                  maxLength: 100,
+                })}
               />
+              {errors.name && (
+                <p className="mt-1 text-primary-500">
+                  {errors.name.type === "required" && "This field is required."}
+                  {errors.name.type === "maxLength" &&
+                    "Max length is 100 characters."}
+                </p>
+              )}
+
+              <input
+                className={inputStyles}
+                type="text"
+                placeholder="EMAIL"
+                {...register("email", {
+                  required: true,
+                  pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                })}
+              />
+              {errors.email && (
+                <p className="mt-1 text-primary-500">
+                  {errors.email.type === "required" &&
+                    "This field is required."}
+                  {errors.email.type === "pattern" && "invalid email address."}
+                </p>
+              )}
+
+              <textarea
+                className={inputStyles}
+                rows={4}
+                cols={50}
+                placeholder="MESSAGE"
+                {...register("message", {
+                  required: true,
+                  maxLength: 2000,
+                })}
+              />
+              {errors.message && (
+                <p className="mt-1 text-primary-500">
+                  {errors.message.type === "required" &&
+                    "This field is required."}
+                  {errors.message.type === "maxLength" &&
+                    "Max length is 2000 characters."}
+                </p>
+              )}
+
               <button
                 type="submit"
-                className="w-1/4 rounded-lg bg-secondary-500 px-20 py-3 transition duration-500 hover:text-white"
+                className="mt-5 rounded-lg bg-secondary-500 px-20 py-3 transition duration-500 hover:text-white"
               >
                 SUBMIT
               </button>
             </form>
           </motion.div>
-        </div>
+          {/* IMAGE */}
+          <motion.div
+            className="relative mt-16 basis-2/5 md:mt-0 "
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ delay: 0.2, duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, y: 50 },
+              visible: { opacity: 1, y: 0 },
+            }}
+          >
+            <div className="md:before:content-evolvetext before:absolute w-full before:-bottom-20 before:-right-10 before:z-[-1]">
+              <img
+                src={image}
+                className="w-full"
+                alt="Contact Us Illustration"
+              />
 
-        <div className="flex w-1/2 items-center justify-center">
-          <img
-            src={image}
-            className="mr-10 w-full max-w-[460px] rounded-lg"
-            alt="Contact Us Illustration"
-          ></img>
+              
+
+            </div>
+          </motion.div>
         </div>
       </motion.div>
     </section>

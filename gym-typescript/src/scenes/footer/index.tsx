6cafab095419fc002dd60e6892e880a9a5c57e92
FeCharaf <felipecharaf@gmail.com>
2025-01-07 19:09:46 -0300

    Footer links

--- gym-typescript/src/scenes/footer/index.tsx
+++ gym-typescript/src/scenes/footer/index.tsx
@@ -1,10 +1,10 @@
-import logo from "@/assets/Logo.png"
+import logo from "@/assets/Logo.png";
 
 const Footer = () => {
   return (
     <footer className="bg-primary-100 py-16">
-      <div className="mx-auto flex h-60 w-4/6 gap-16">
-        <div className="basis-1/2">
+      <div className="mx-auto w-5/6 justify-center gap-16 md:flex">
+        <div className="mt-16 basis-1/2 md:mt-0">
           <img src={logo} alt="logo icon" />
           <p className="my-5">
             Lorem vitae ut augue auctor faucibus eget eget ut libero. Elementum
@@ -14,17 +14,17 @@ const Footer = () => {
           <p>© Evogym All Rights Reserved.</p>
         </div>
 
-        <div>
+        <div className="mt-16 basis-1/4 md:mt-0">
           <h4 className="font-bold">Links</h4>
-          <p>Massa orci senectus</p>
+          <p className="my-5">Massa orci senectus</p>
           <p className="my-5">Et gravida id et etiam</p>
           <p>Ullamcorper vivamus</p>
         </div>
 
-        <div>
+        <div className="mt-16 basis-1/4 md:mt-0">
           <h4 className="font-bold">Contact Us</h4>
           <p className="my-5">Tempus metus mattis risus volutpat egestas.</p>
-          <p>(11)3425-6825</p>
+          <p>(11) 1234-5678</p>
         </div>
       </div>
     </footer>
